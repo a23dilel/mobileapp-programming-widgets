f3c59ac0e32ad4a225ae7fd54759203299773e99
a23dilel <a23dilel@student.his.se>
2024-04-06 19:32:50 +0200

    Added a button function to MainActivity.java for adding a text to TextView.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // created a button
+        Button button = findViewById(R.id.button);
+
+        // listening if user click a button
+        button.setOnClickListener( new View.OnClickListener() {
+                    // when user pressed onclick
+                    public void onClick(View v) {
+                        // add text in textview
+                        TextView textView = findViewById(R.id.TextViewAboutMe);
+                        textView.setText(R.string.aboutMe);
+                    }
+        });
     }
 }
